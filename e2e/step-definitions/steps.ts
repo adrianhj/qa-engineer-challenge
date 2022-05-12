e2a1410ea9a871b1d7561f378fffae63e3aaef6a
Adrian Hjertstedt <adrian@ldms.com>
2022-05-12 14:23:42 +0100

    feat: bootstrap WebdriverIO

---
+++ e2e/step-definitions/steps.ts
@@ -0,0 +1,20 @@
+import { Given, Then, When } from "@wdio/cucumber-framework";
+import LoginPage from "../pages/login.page";
+import SecurePage from "../pages/secure.page";
+
+const pages = {
+  login: LoginPage,
+};
+
+Given(/^I am on the (\w+) page$/, async (page) => {
+  await pages[page].open();
+});
+
+When(/^I login with (\w+) and (.+)$/, async (username, password) => {
+  await LoginPage.login(username, password);
+});
+
+Then(/^I should see a flash message saying (.*)$/, async (message) => {
+  await expect(SecurePage.flashAlert).toBeExisting();
+  await expect(SecurePage.flashAlert).toHaveTextContaining(message);
+});
